cd4200c77cf09054433318a972075eb4b89594dc
Gill <asguilift@hotmail.com>
2022-07-27 16:09:52 -0300

    InterfaceId Function

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-
-async function test(object) {  
-  const prepareData = e => `${e.name}(${e.inputs.map(e => e.type)})`
-  // Encode function selector (assume web3 is globally available)
-  const encodeSelector = f => ethers.utils.id(f).slice(0,10);
-  // Parse ABI and encode its functions
-  const output = object
-    .filter(e => e.type === "function")
-    .flatMap(e => `${encodeSelector(prepareData(e))}`);
-  // Xor the output values and convert to hex
-  console.log(ethers.utils.hexlify(output.reduce((prev, cur) => prev ^ cur)));
+const { ethers } = require("ethers");
+const interfaceId = (object) =>  {
+  try {
+    const prepareData = e => `${e.name}(${e.inputs.map(e => e.type)})`
+    // Encode function selector
+    const encodeSelector = f => ethers.utils.id(f).slice(0,10);
+    // Parse ABI and encode its functions
+    const output = object
+      .filter(e => e.type === "function")
+      .flatMap(e => `${encodeSelector(prepareData(e))}`);
+    // Xor the output values and convert to hex
+    return ethers.utils.hexlify(output.reduce((prev, cur) => prev ^ cur));
+  } catch (err) { return err; }
 }
+
+module.exports.interfaceId = interfaceId;
