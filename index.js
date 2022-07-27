2d6e439fadc58974fa6de7757be83794e36d4d67
Alexandro T. Netto <alex.t.netto@gmail.com>
2022-07-27 16:56:02 -0300

    version 1.0.0

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
-const { ethers } = require("ethers");
-const interfaceId = (object) =>  {
+const { keccak256 } = require('js-sha3')
+
+function interfaceIdFromABI(abi) {
   try {
     const prepareData = e => `${e.name}(${e.inputs.map(e => e.type)})`
-    // Encode function selector
-    const encodeSelector = f => ethers.utils.id(f).slice(0,10);
+
+    const encodeSelector = f => "0x" + keccak256(f).slice(0, 8);
+
     // Parse ABI and encode its functions
-    const output = object
+    const functionSelectors = abi
       .filter(e => e.type === "function")
       .flatMap(e => `${encodeSelector(prepareData(e))}`);
+
     // Xor the output values and convert to hex
-    return ethers.utils.hexlify(output.reduce((prev, cur) => prev ^ cur));
-  } catch (err) { return err; }
+    const interfaceId = "0x" + functionSelectors.reduce((prev, cur) => prev ^ cur).toString(16);
+
+    return interfaceId;
+  } catch (err) { console.log(err); }
 }
 
-module.exports.interfaceId = interfaceId;
+module.exports.interfaceIdFromABI = interfaceIdFromABI
